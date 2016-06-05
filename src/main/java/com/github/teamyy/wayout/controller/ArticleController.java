5ebad90719dac89922974894a3c0ee857b859a17
skyoo2003 <skyoo2003@gmail.com>
2016-06-05 13:28:08 +0900

    컨트롤러, 서비스 클래스 초기화

---
+++ src/main/java/com/github/teamyy/wayout/controller/ArticleController.java
@@ -0,0 +1,7 @@
+package com.github.teamyy.wayout.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ArticleController {
+}
---
+++ src/main/java/com/github/teamyy/wayout/service/ArticleService.java
@@ -0,0 +1,7 @@
+package com.github.teamyy.wayout.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+}
