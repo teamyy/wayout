aa497a8efe2a5289dd6f3526b6463c34643225c1
lukas.sky <lukas.sky@daumkakao.com>
2016-06-05 12:47:42 +0900

    Initialize project

---
+++ src/main/java/com/github/teamyy/wayout/WayoutApplication.java
@@ -0,0 +1,11 @@
+package com.github.teamyy.wayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayoutApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WayoutApplication.class, args);
+    }
+}
